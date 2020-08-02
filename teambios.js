5c13b4429b4f8f1107b04ae210a3d830670ddd8f
rowedaniel <48768975+rowedaniel@users.noreply.github.com>
2020-08-02 10:23:23 -0700

    Update and rename navbar.js to teambios.js

---
+++ teambios.js
@@ -0,0 +1,9 @@
+//Add https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.pkgd.js as external script
+function myFunction(elemId) {
+  var x = document.getElementById(elemId);
+  if (x.style.display === "none") {
+	x.style.display = "block";
+  } else {
+	x.style.display = "none";
+  }
+}
