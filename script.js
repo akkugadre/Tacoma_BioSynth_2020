76296d8b4d3532fb6069fbb4e04a5d4b0751fdbd
rowedaniel <48768975+rowedaniel@users.noreply.github.com>
2020-08-02 10:23:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 var navbar = document.getElementById("navbar");
-//navbar.innerHTML = ``;
+navbar.innerHTML = `<body>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+   <div id=navbar>
+     <img src="https://cdn.discordapp.com/attachments/711039492688969790/735387388444147762/unknown.png"></img>
+      <dl>
+         <a href=https://codepen.io/MaxAllen/pen/QWyZrRy>
+            <dt>Home</dt>
+         </a>
+         <a href=https://codepen.io/MaxAllen/pen/QWyZrRy>
+            <dt>Team</dt>
+         </a>
+         <a href=https://codepen.io/MaxAllen/pen/QWyZrRy>
+            <dd>Team&nbsp;Members</dd>
+         </a>
+         <a href=https://codepen.io/MaxAllen/pen/QWyZrRy>
+            <dd>Collaborations</dd>
+         </a>
+         <a href=https://codepen.io/MaxAllen/pen/QWyZrRy>
+            <dt>Project</dt>
+         </a>
+         <a href=https://codepen.io/MaxAllen/pen/QWyZrRy>
+            <dd>Description</dd>
+         </a>
+         <a href=https://codepen.io/MaxAllen/pen/QWyZrRy>
+            <dd>Research</dd>
+         </a>
+         <a href=https://codepen.io/MaxAllen/pen/QWyZrRy>
+            <dd>Contributions</dd>
+         </a>
+         <a href=https://codepen.io/MaxAllen/pen/QWyZrRy>
+            <dd>Attributions</dd>
+         </a>
+      </dl>
+   </div>
+</body>`;
 var body = document.body,
     html = document.documentElement;
 
