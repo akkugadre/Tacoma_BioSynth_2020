d4dc719d20b599f26cd19b123ab0b812277176e6
rowedaniel <48768975+rowedaniel@users.noreply.github.com>
2020-08-10 18:47:28 -0700

    Made navbar not absurdly long

--- script.js
+++ script.js
@@ -33,11 +33,11 @@ navbar.innerHTML = `
    `;
 var body = document.body,
     html = document.documentElement;
-
+/*
 var height = Math.max( body.scrollHeight, body.offsetHeight, 
                        html.clientHeight, html.scrollHeight, html.offsetHeight );
 navbar.style.height = height+"px";
-console.log(height+"px");
+console.log(height+"px");*/
 
 
 
